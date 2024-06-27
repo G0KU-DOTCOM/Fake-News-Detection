c5fc3486a2eb943bc4d385fbc2fe8d1baa79a9ca
Agob Haroun <Agob699@live.no>
2024-06-27 19:59:19 +0200

    applied vectorization to the data

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ data = pd.read_csv('fake_or_real_news.csv')
 
 data["fake"] = data["label"].apply(lambda x: 0 if x == "REAL" else 1)
 data = data.drop("label", axis=1)
-print(data.head())
+#print(data.head())
+
+x, y = data['text'], data['fake']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+len(x_train), len(x_test)
+
+vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+x_train_vectorized = vectorizer.fit_transform(x_train)
+x_test_vectorized = vectorizer.transform(x_test)
+
+
+
