0035285fe1061fe20e1f4ce8aae0ae5276cebed1
Agob Haroun <Agob699@live.no>
2024-06-28 09:38:21 +0200

    implementing hyperpar, pipeline

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer #representign the articles as numbers
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from googletrans import Translator
 
 data = pd.read_csv('fake_or_real_news.csv')
 
@@ -12,9 +15,19 @@ data = data.drop("label", axis=1)
 #print(data.head())
 
 x, y = data['text'], data['fake']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 #len(x_train), len(x_test)
+
+pipeline = Pipeline([
+('vectorizer', TfidfVectorizer(stop_words='english', max_df=0.7)),
+    ('clf', LinearSVC())
+])
+
+param_grid = {
+    'vectorizer_max_df': [0.7, 0.9, 1.0],
+    'clf_C': [0.1, 1, 10, 100, 1000]
+}
 
 vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 x_train_vectorized = vectorizer.fit_transform(x_train)
